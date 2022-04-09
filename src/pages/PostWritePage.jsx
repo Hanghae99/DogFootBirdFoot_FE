1bbd21c2c4ec8551739949cd7a5eba684f7d6baf
YeonJae <duswo9595@naver.com>
2022-04-09 16:04:22 +0900

    feat: 글 작성페이지 레이아웃작업 완료

---
+++ src/pages/PostWritePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PostWrite from "../components/postWrite/PostWrite";
+
+const PostWritePage = (props) => {
+  return (
+    <>
+      <PostWrite />
+    </>
+  );
+};
+
+export default PostWritePage;
--- src/shared/App.jsx
+++ src/shared/App.jsx
@@ -1,10 +1,11 @@
 import PostDetailPage from "../pages/PostDetailPage";
 import Header from "./header/Header";
 import { Route } from "react-router-dom";
-import Mypage from "../pages/Mypage";
-import Mainpage from "../pages/Mainpage";
-import Login from "../pages/Login";
+import Mypage from "../pages/MyPage";
+import Mainpage from "../pages/MainPage";
+import LoginPage from "../pages/LoginPage";
 import Signuppage from "../pages/SignupPage";
+import PostWritePage from "../pages/PostWritePage";
 
 function App() {
   return (
@@ -12,8 +13,9 @@ function App() {
       <Header />
       <Route path="/" exact component={Mainpage} />
       <Route path="/mypage" exact component={Mypage} />
-      <Route path="/login" exact component={Login} />
+      <Route path="/login" exact component={LoginPage} />
       <Route path="/postdetail" exact component={PostDetailPage} />
+      <Route path="/postwrite" exact component={PostWritePage} />
       <Route path="/signup" exact component={Signuppage} />
     </>
   );
